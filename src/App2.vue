79f814ff229c22495d8b13c6d4ae03fbd0630ad3
David Heidari <davidheidari2@gmail.com>
2024-10-05 18:01:26 +0200

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,103 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "David",
+      status: "active",
+      tasks: ["Task-one", "Task-two", "task-three"],
+      link: "https://google.com",
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a> <br />
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<!-- 
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const name = ref("David Heidari");
+const status = ref("active");
+const tasks = ref(["task-1", "task-2", "task-3"]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log("Error fetching tasks.");
+  }
+});
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task: </label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span> {{ task }}</span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+  </ul>
+
+   <button v-on:click="toggleStatus">Change Status</button> -->
+<!-- <button @click="toggleStatus">Change Status</button>
+</template>  -->
