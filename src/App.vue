98fcaaeb31879e2285d6fcf44772f43f4110a25b
David Heidari <davidheidari2@gmail.com>
2024-10-08 08:40:18 +0200

    add a jobs and add jobs page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
-import Hero from "./components/Hero.vue";
-import HomeCrads from "./components/HomeCards.vue";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Navbar />
-  <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
-  <Hero />
-  <HomeCrads />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from "../components/Hero.vue";
+import HomeCrads from "../components/HomeCards.vue";
+import JobListings from "../components/JobListings.vue";
+</script>
+
+<template>
+  <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
+  <Hero />
+  <HomeCrads />
+  <JobListings :limit="3" :showButton="true" />
+</template>
